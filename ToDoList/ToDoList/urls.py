816667e49133d5a6dd14a8e1cb3f6c160dadf938
syuxin2019 <519802856@qq.com>
2020-04-22 09:13:35 +0800

    useless modification

--- ToDoList/ToDoList/urls.py
+++ ToDoList/ToDoList/urls.py
@@ -1,4 +1,4 @@
-"""ToDoList URL Configuration
+"""ToDoList URL Configuration//
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.11/topics/http/urls/
